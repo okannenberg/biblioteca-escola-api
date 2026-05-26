e9204e43f4ad58de8f4afba3d93c599275b2ba3c
Bruno Gustavo Kannenberg <brunokannenberg794@gmail.com>
2026-05-26 10:55:36 -0300

    First commit: Initial Project Structure

---
+++ src/main/java/com/project/biblioteca_escola_api/BibliotecaEscolaApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.biblioteca_escola_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaEscolaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaEscolaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/biblioteca_escola_api/BibliotecaEscolaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.biblioteca_escola_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaEscolaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
