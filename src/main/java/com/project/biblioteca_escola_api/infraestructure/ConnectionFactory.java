daeeb2f508804c79efd56b9843226db22d55b4c2
Bruno Gustavo Kannenberg <brunokannenberg794@gmail.com>
2026-06-01 10:04:03 -0300

    Ajuste: Arrumado a porta de conexão com o banco de dados

--- src/main/java/com/project/biblioteca_escola_api/infraestructure/ConnectionFactory.java
+++ src/main/java/com/project/biblioteca_escola_api/infraestructure/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
 
     private static Connection connection = null;
 
-    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca_escola?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3386/biblioteca_escola?useSSL=false&serverTimezone=UTC";
 
     private static final String USER = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "root";
     private static final String PASSWORD = System.getenv("DB_PASSWORD");
