fbeac5003f8dd5dc61de279aad76e3740c9300b3
Bruno Gustavo Kannenberg <brunokannenberg794@gmail.com>
2026-06-01 08:17:49 -0300

    Feat: Criado a classe model de livro.

--- src/main/java/com/project/biblioteca_escola_api/model/Livro.java
+++ src/main/java/com/project/biblioteca_escola_api/model/Livro.java
@@ -1,4 +1,55 @@
 package com.project.biblioteca_escola_api.model;
 
 public class Livro {
+    private long id;
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+    public Livro(long id, String titulo, String autor, boolean disponivel) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = disponivel;
+    }
+
+    public Livro(String titulo, String autor, boolean disponivel) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = disponivel;
+    }
+
+    public Livro() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }
