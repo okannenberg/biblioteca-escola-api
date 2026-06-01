6707facbfdb26bfa5714db3cd70d6cbd7b3f51d6
Bruno Gustavo Kannenberg <brunokannenberg794@gmail.com>
2026-06-01 13:04:41 -0300

    Feat: Criado a classe LivroDAO

--- src/main/java/com/project/biblioteca_escola_api/dao/livro/LivroDAO.java
+++ src/main/java/com/project/biblioteca_escola_api/dao/livro/LivroDAO.java
@@ -1,4 +1,16 @@
 package com.project.biblioteca_escola_api.dao.livro;
 
+import com.project.biblioteca_escola_api.model.Livro;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
 public interface LivroDAO {
+    Livro saveLivro(Livro livro) throws SQLException;
+    List<Livro> findAllLivros() throws SQLException;
+    Optional<Livro> findLivroPorId(Long id) throws SQLException;
+    boolean existsLivroPorId(Long id) throws SQLException;
+    void atualizarLivro(Livro livro) throws SQLException;
+    void deletarLivroPorId(Long id) throws SQLException;
 }
